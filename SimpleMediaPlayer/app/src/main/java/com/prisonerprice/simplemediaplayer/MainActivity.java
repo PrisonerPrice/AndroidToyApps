3d9968ca68c04043cdfbaf79486aae2f8358c987
Hangbo <zhzhanghangbo@hotmail.com>
2020-02-13 16:49:16 -0500

    Change code to googlecodelab's and bugs fixed...

--- SimpleMediaPlayer/app/src/main/java/com/prisonerprice/simplemediaplayer/MainActivity.java
+++ SimpleMediaPlayer/app/src/main/java/com/prisonerprice/simplemediaplayer/MainActivity.java
@@ -2,11 +2,13 @@ package com.prisonerprice.simplemediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
@@ -14,31 +16,101 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
 
+    private PlayerView playerView;
     private SimpleExoPlayer player;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        player = new SimpleExoPlayer.Builder(this).build();
-        PlayerView playerView = new PlayerView(this);
-        playerView.setPlayer(player);
-        player.setPlayWhenReady(true);
-
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "SimpleMediaPlayer"));
-        MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(Uri.parse("asset:///canon_in_d.mp3"));
-        player.prepare(mediaSource);
+        playerView = findViewById(R.id.exo_player);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        player.release();
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        player = new SimpleExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
+
+        Uri uri = Uri.parse("asset:///sample.mp4");
+        MediaSource mediaSource = buildMediaSource(uri);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+        player.prepare(mediaSource, false, false);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        // These factories are used to construct two media sources below
+        DataSource.Factory dataSourceFactory =
+                new DefaultDataSourceFactory(this, "exoplayer-codelab");
+        ProgressiveMediaSource.Factory mediaSourceFactory =
+                new ProgressiveMediaSource.Factory(dataSourceFactory);
+
+        // Create a media source using the supplied URI
+        MediaSource mediaSource1 = mediaSourceFactory.createMediaSource(uri);
+
+        // Additionally create a media source using an MP3
+        Uri audioUri = Uri.parse("asset:///canon_in_d.mp3");
+        MediaSource mediaSource2 = mediaSourceFactory.createMediaSource(audioUri);
+
+        return new ConcatenatingMediaSource(mediaSource1, mediaSource2);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
     }
 }
