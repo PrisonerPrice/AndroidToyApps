8e2a708a4071ba4780172ba6657b690a96464b36
Hangbo <zhzhanghangbo@hotmail.com>
2020-02-12 23:48:55 -0500

    Add simple playerView

--- SimpleMediaPlayer/app/src/main/java/com/prisonerprice/simplemediaplayer/MainActivity.java
+++ SimpleMediaPlayer/app/src/main/java/com/prisonerprice/simplemediaplayer/MainActivity.java
@@ -2,13 +2,42 @@ package com.prisonerprice.simplemediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SimpleExoPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player = new SimpleExoPlayer.Builder(this).build();
+        PlayerView playerView = new PlayerView(this);
+        playerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this, Util.getUserAgent(this, "SimpleMediaPlayer"));
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse("android.resources://com.prisonerprice.simplemediaplayer/raw/demo"));
+        player.prepare(mediaSource);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        player.release();
     }
 }
